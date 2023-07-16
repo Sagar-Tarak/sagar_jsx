5dd24aca26c9980abcbf95ca6cd55eb33d50624d
Sagar Tarak <114102151+Sagar-Tarak@users.noreply.github.com>
2023-07-16 20:15:25 +0530

    sagar.jsx first commite

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+import Wrapper from "./Wrapper";
+import flag from "../assets/indian-flag.png";
+import pe1 from "../assets/sec-2-p-e-1.png";
+import pe2 from "../assets/sec-2-p-e-2.png";
+import Div from "./Div";
+
+const AboutMe = () => {
+    const { scrollY } = useScroll();
+    const y1 = useTransform(scrollY, [0, 500], [100, 0], { clamp: false });
+    const y2 = useTransform(scrollY, [0, 500], [0, -200], { clamp: false });
+    return (
+        <div
+            id="about"
+            className="w-full py-[50px] md:py-[100px] bg-white text-black relative overflow-hidden"
+        >
+            {/* BACKGROUND ELEMENTS START */}
+            <span className="sec-2-bg-gradient" />
+            <motion.img className="sec-2-p-e-1" style={{ y: y1 }} src={pe1} />
+            <motion.img className="sec-2-p-e-2" style={{ y: y2 }} src={pe2} />
+            {/* BACKGROUND ELEMENTS END */}
+
+            <Wrapper>
+                {/* HEADING START */}
+                <Div className="text-[40px] md:text-[90px] 2xl:text-[120px] leading-[52px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase mb-[25px] md:mb-[40px] 2xl:mb-[60px] flex flex-col relative">
+                    <span>A Frontend Web Desinger</span>
+                    <span className="flex items-center gap-2">
+                        <span>Based</span>
+                        <img
+                            src={flag}
+                            alt=""
+                            className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+                        />
+                        <span>In India</span>
+                    </span>
+                </Div>
+                {/* HEADING END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                passionate about creating visually stunning and highly functional websites. With several years of experience in the industry, I specialize in crafting engaging user experiences through clean and efficient code.
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                As a frontend web developer, I am dedicated to transforming ideas into captivating digital solutions. My expertise lies in converting design mockups into pixel-perfect, responsive web interfaces that work seamlessly across different devices and browsers. I have a keen eye for detail, ensuring that every element of a website is flawlessly executed and visually appealing.
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] relative">
+                My skill set includes proficiency in HTML, CSS, JavaScript, and various frontend frameworks such as React. I stay up to date with the latest industry trends and best practices, continuously honing my skills to deliver cutting-edge solutions that meet the demands of modern web development.
+                </Div>
+                {/* PARAGRAPH END */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default AboutMe;
